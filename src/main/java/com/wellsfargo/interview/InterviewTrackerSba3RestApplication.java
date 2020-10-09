e7ea60664a9442a3657d18ce8c574e895012a6d1
Rathish <wellsfargofsd56@iiht.tech>
2020-10-09 18:26:18 +0530

    Initial Commit

---
+++ src/main/java/com/wellsfargo/interview/InterviewTrackerSba3RestApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTrackerSba3RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTrackerSba3RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellsfargo/interview/InterviewTrackerSba3RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTrackerSba3RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
