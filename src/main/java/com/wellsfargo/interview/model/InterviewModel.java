c5f1f290c939ca713dd9346c1a45d74647b6c052
Rathish <wellsfargofsd56@iiht.tech>
2020-10-12 14:53:12 +0530

    Updated Project

--- src/main/java/com/wellsfargo/interview/model/InterviewModel.java
+++ src/main/java/com/wellsfargo/interview/model/InterviewModel.java
@@ -3,8 +3,6 @@ package com.wellsfargo.interview.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
--- src/main/java/com/wellsfargo/interview/model/UserModel.java
+++ src/main/java/com/wellsfargo/interview/model/UserModel.java
@@ -1,8 +1,6 @@
 package com.wellsfargo.interview.model;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
